c5801f5d0540d898b53643a045536e4c33f2ffbb
PhummyM <phummym@gmail.com>
2026-06-25 14:17:41 +0200

    Add files via upload

---
+++ conversion.py
@@ -0,0 +1,24 @@
+#Declare the following variables
+num1 = 99.23
+num2 = 23
+num3 = 150
+string1 = "100"
+
+#Convert them as follows:
+#Convert num1 to an integer
+num1_int = int(num1)
+
+#Convert num2 to a float
+num2_float = float(num2)
+
+#Convert num3 to a string
+num3_str = str(num3)
+
+#Convert string1 to an integer
+string1_int = int(string1)
+
+#Print out all variables on a separate line
+print(num1_int)
+print(num2_float)
+print(num3_str)
+print(string1_int)
---
+++ details.py
@@ -0,0 +1,23 @@
+#Prompt the user to input their name
+name = input("Please enter your name: ")
+#Store the user's name in a variable called "name"
+name = ("Rose")
+print(name)
+#Prompt the user to input their age
+age = input("Please enter your age: ")
+#Store the user's age in a variable called "age"
+age = ("38")
+print(age)
+#Prompt the user to input their house number
+house_number = input("Please enter your house number: ")
+#Store the user's house number in a variable called "house_number"
+house_number = ("9224")
+print(house_number)
+#Prompt the user to input their street name
+street_name = input("Please enter your street name: ")
+#Store the user's street name in a variable called "street_name"
+street_name = ("Tswelopele Street")
+print(street_name)
+
+#Print out a single sentence that includes the user's name, age, house number, and street name
+print(f"My name is {name}, I am {age} years old, and I live at {house_number} {street_name}.")
